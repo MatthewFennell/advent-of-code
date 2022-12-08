0bd9bd1e7f4502414673a59bda68c7bc470f9b1a
MatthewFennell <m.fennell@live.co.uk>
2022-12-08 21:30:14 +0000

    Day 8 part 2

--- 2022/8_answer.py
+++ 2022/8_answer.py
@@ -73,4 +73,94 @@ def part_one():
     num_trees = total_num_visible_trees(visible_trees)
     print("Part One:", num_trees)
 
+def time_since_seen_larger(what_ive_seen, column_number, tree_height, is_reverse, total_length):
+    last_seen_keys = what_ive_seen.keys()
+    keys_in_dict = [x for x in last_seen_keys if x in what_ive_seen and x >= tree_height]
+    if len(keys_in_dict) == 0:
+        if is_reverse:
+            return total_length - column_number - 1
+        return column_number
+    if is_reverse:
+        return min(what_ive_seen[x] for x in keys_in_dict) - column_number
+    return column_number - max(what_ive_seen[x] for x in keys_in_dict)
+
+def find_max_score(results):
+    max_score = 0
+    for key in results.keys():
+        for key_two in results[key].keys():
+            val = results[key][key_two]
+            result = val['view_left'] * val['view_right'] * val['view_above'] * val['view_below']
+            if result > max_score:
+                max_score = result
+    return max_score
+
+def part_two():
+    lines = get_number_list('8_input.txt')
+    results = {}
+    for row in range(0, len(lines)):
+        results[row] = {}
+        last_seen = {}
+
+        for column in range(0, len(lines[row])):
+            results[row][column] = {}
+            tree_height = lines[row][column]            
+            
+            # left to right
+            if column == 0:
+                results[row][column]['view_left'] = 0
+            elif lines[row][column] <= lines[row][column-1]:
+                results[row][column]['view_left'] = 1
+            else:
+                num_trees_can_see_to_left = time_since_seen_larger(last_seen, column, tree_height, False, len(lines[row]))
+                results[row][column]['view_left'] = num_trees_can_see_to_left
+            last_seen[tree_height] = column
+
+        last_seen = {}
+        for column in range(len(lines[row])-1, -1, -1):
+            tree_height = lines[row][column]            
+            
+            # right to left
+            if column == len(lines[row])-1:
+                results[row][column]['view_right'] = 0
+            elif lines[row][column] <= lines[row][column+1]:
+                results[row][column]['view_right'] = 1
+            else:
+                num_trees_can_see_to_right = time_since_seen_larger(last_seen, column, tree_height, True, len(lines[row]))
+                results[row][column]['view_right'] = num_trees_can_see_to_right
+            last_seen[tree_height] = column
+
+        
+    for column in range(0, len(lines[0])):
+        last_seen = {}
+        for row in range(0, len(lines)):
+            tree_height = lines[row][column]            
+            
+            # top_to_bottom
+            if row == 0:
+                results[row][column]['view_above'] = 0
+            elif lines[row][column] <= lines[row-1][column]:
+                results[row][column]['view_above'] = 1
+            else:
+                num_trees_can_see_above = time_since_seen_larger(last_seen, row, tree_height, False, len(lines))
+                results[row][column]['view_above'] = num_trees_can_see_above
+            last_seen[tree_height] = row
+
+        last_seen = {}
+        for row in range(len(lines)-1, -1, -1):
+            tree_height = lines[row][column]            
+            
+            # bottom_to_top[]
+            if row == len(lines)-1:
+                results[row][column]['view_below'] = 0
+            elif lines[row][column] <= lines[row+1][column]:
+                results[row][column]['view_below'] = 1
+            else:
+                num_trees_can_see_above = time_since_seen_larger(last_seen, row, tree_height, True, len(lines))
+                results[row][column]['view_below'] = num_trees_can_see_above
+            last_seen[tree_height] = row
+
+    max_score = find_max_score(results)
+    print("Part Two:", max_score)
+
 part_one()
+part_two()
